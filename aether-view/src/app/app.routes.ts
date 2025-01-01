1e096132bc9f66e5d3f6c76c137980dd86db0596
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-02 00:00:33 +0100

    Refactor app component to include HeaderComponent and update routing for MovieDetails

--- aether-view/src/app/app.routes.ts
+++ aether-view/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { DiscoverComponent } from './pages/discover/discover.component';
 import { TvDetailsComponent } from './pages/tv-details/tv-details.component';
 import { AppComponent } from './app.component';
 import { RecommendationComponent } from './pages/recommendation/recommendation.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
 export const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'recommendation', component: RecommendationComponent },
   { path: 'discover', component: DiscoverComponent},
   { path: 'tv/:id', component: TvDetailsComponent },
+  { path: 'movie/:id', component: MovieDetailsComponent }
 ];
