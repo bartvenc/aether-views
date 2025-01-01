a0acda7b2e2bf16d72d85e4093341196e3edaf3c
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-02 00:01:23 +0100

    Add trending and upcoming movies functionality with genre support in TmdbService

--- aether-view/src/app/services/tmdb.service.ts
+++ aether-view/src/app/services/tmdb.service.ts
@@ -4,6 +4,9 @@ import { Series } from '../interfaces/series';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 import { forkJoin, Observable, switchMap, tap } from 'rxjs';
+import { Movie } from '../interfaces/movies';
+import { Genre, Person } from '../interfaces/common-interfaces';
+import { Studio } from '../../../public/assets/studios';
 
 @Injectable({ providedIn: 'root' })
 export class TmdbService {
@@ -15,17 +18,133 @@ export class TmdbService {
 
   // Convert the HTTP Observable directly to a Signal
   readonly popularSeries: Signal<Series[]> = toSignal(
+    this.http.get<{ results: Series[] }>(`${this.baseUrl}/tv/popular?api_key=${this.apiKey}`).pipe(
+      map(response => response.results)
+    ),
+    { initialValue: [] }
+  );
+
+
+  // Convert the HTTP Observable directly to a Signal
+  readonly trendingSeries: Signal<Series[]> = toSignal(
     this.http.get<{ results: Series[] }>(`${this.baseUrl}/trending/tv/week?api_key=${this.apiKey}`).pipe(
       map(response => response.results)
     ),
     { initialValue: [] }
   );
 
+  readonly trendingMovies: Signal<Movie[]> = toSignal(
+    this.http.get<{ results: Movie[] }>(`${this.baseUrl}/trending/movie/week?api_key=${this.apiKey}`).pipe(
+      map(response => response.results)
+    ),
+    { initialValue: [] }
+  );
+
+
+  readonly upcomingMovies: Signal<Movie[]> = toSignal(
+    this.http.get<{ results: Movie[] }>(`${this.baseUrl}/movie/upcoming?api_key=${this.apiKey}`).pipe(
+      map(response => response.results)
+    ),
+    { initialValue: [] }
+  );
+
+
+  readonly movieGenres: Signal<Genre[]> = toSignal(
+    this.http.get<{ genres: Genre[] }>(`${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}`).pipe(
+      map(response => response.genres)
+    ),
+    { initialValue: [] }
+  );
+
+
+  getImageUrl(item: Series | Movie | Studio | Genre | Person | null | undefined): string | null {
+    if (!item) return null;
+
+    if (this.isSeries(item)) {
+      return this.getPosterUrl(item.poster_path);
+    }
+
+    if (this.isMovie(item)) {
+      return this.getPosterUrl(item.poster_path);
+    }
+
+    if (this.isStudio(item)) {
+      return this.getStudioLogoUrl(item);
+    }
+
+    if (this.isGenre(item)) {
+      return this.getPosterUrl(item.posterUrl);
+    }
+
+    if (this.isPerson(item)) {
+      return this.getPosterUrl(item.profile_path);
+    }
+
+    console.warn('Unknown item type:', item);
+    return null;
+  }
+
+  // Type guards
+   isSeries(item: any): item is Series {
+    return this.hasProperty<Series>(item, 'first_air_date', 'string');
+  }
+   isMovie(item: any): item is Movie {
+    return this.hasProperty<Movie>(item, 'release_date', 'string');
+  }
+   isStudio(item: any): item is Studio {
+    return this.hasProperty<Studio>(item, 'logo_path', 'string') && this.hasProperty<Studio>(item, 'name', 'string');
+  }
+   isGenre(item: any): item is Genre {
+    return this.hasProperty<Genre>(item, 'id', 'number') 
+    && this.hasProperty<Genre>(item, 'name', 'string') 
+    && !('logo_path' in item)
+    && !('profile_path' in item);
+  }
+
+  isPerson(item: any): item is Person {
+    return this.hasProperty<Person>(item, 'profile_path', 'string');
+  }
+
+  // General utility to check property types
+  private hasProperty<T>(obj: any, key: keyof T, type: string): boolean {
+    return obj?.[key] !== undefined && typeof obj[key] === type;
+  }
+
+  getPosterUrl(path: string | null | undefined): string {
+    return path ? `https://image.tmdb.org/t/p/w500${path}` : '';
+  }
+
+  getBackDrop(path: string | null | undefined): string | null {
+    return path ? `https://image.tmdb.org/t/p/original${path}` : null;
+  }
+
+  getStudioLogoUrl(studio: Studio | undefined): string | null {
+    return studio ? `https://image.tmdb.org/t/p/w780_filter(duotone,ffffff,bababa)/${studio.logo_path}` : null;
+  }
+
+  getTrailerUrl(series: Series  | Movie | undefined): string | null {
+    const videos = series?.videos?.results;
+    return videos?.length ? `https://www.youtube.com/watch?v=${videos[0].key}` : null;
+  }
+
   getSeriesDetailsById(seriesId: number): Observable<Series> {
     return this.http.get<Series>(`${this.baseUrl}/tv/${seriesId}`, {
       params: {
         api_key: this.apiKey,
         append_to_response: 'images,videos,keywords,recommendations,similar,episode_groups,credits,content_ratings'
+      }
+    }).pipe(
+      tap(response => {
+        console.log('Full series details:', response);
+      })
+    );
+  }
+
+  getMovieDetailsById(movieId: number): Observable<Movie> {
+    return this.http.get<Movie>(`${this.baseUrl}/movie/${movieId}`, {
+      params: {
+        api_key: this.apiKey,
+        append_to_response: 'images,videos,keywords,recommendations,similar,credits,releases'
       }
     }).pipe(
       tap(response => {
@@ -53,14 +172,14 @@ export class TmdbService {
 
   getSeriesRecommendations(series: Series): Observable<Series[]> {
     console.log('Getting recommendations for:', series.name, 'ID:', series.id);
-    
+
     return this.http.get<Series>(`${this.baseUrl}/tv/${series.id}`, {
       params: { api_key: this.apiKey }
     }).pipe(
       tap(originalSeries => {
         console.log('Original series genres:', originalSeries.genres);
       }),
-      switchMap(originalSeries => 
+      switchMap(originalSeries =>
         this.http.get<{ results: Series[] }>(`${this.baseUrl}/tv/${series.id}/recommendations`, {
           params: { api_key: this.apiKey }
         }).pipe(
@@ -69,7 +188,7 @@ export class TmdbService {
           }),
           map(response => {
             const matchingRecs = response.results.filter(rec => {
-              const hasMatchingGenre = rec.genre_ids?.some(genreId => 
+              const hasMatchingGenre = rec.genre_ids?.some(genreId =>
                 originalSeries.genres?.some(genre => genre.id === genreId)
               );
               console.log(
@@ -92,18 +211,7 @@ export class TmdbService {
     );
   }
 
-  getPosterUrl(path: string | null | undefined): string {
-    return path ? `https://image.tmdb.org/t/p/w500${path}` : '';
-  }
 
-  getBackDrop(path: string | null | undefined): string | null {
-    return path ? `https://image.tmdb.org/t/p/original${path}` : null;
-  }
-
-  getTrailerUrl(series: Series | undefined): string | null {
-    const videos = series?.videos?.results;
-    return videos?.length ? `https://www.youtube.com/watch?v=${videos[0].key}` : null;
-  }
 }
-  
+
 
