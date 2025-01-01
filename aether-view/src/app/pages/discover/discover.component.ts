f34379d5b8cb57757599ad56ad765acb90357e4b
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-02 00:01:07 +0100

    Enhance discover component to display trending movies, upcoming movies, genres, and studios with improved slider integration

--- aether-view/src/app/pages/discover/discover.component.ts
+++ aether-view/src/app/pages/discover/discover.component.ts
@@ -1,52 +1,46 @@
 
 import { Component, inject, Signal, signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Series } from '../../interfaces/series';
+import { Movie } from '../../interfaces/movies';
 import { TmdbService } from '../../services/tmdb.service';
-import { RouterLink } from '@angular/router';
 import { Swiper } from 'swiper';
+import { SliderComponent } from '../../components/slider/slider.component';
+import { MOVIE_GENRES, SERIES_GENRES } from '../../../../public/assets/genres';
+import { STUDIOS } from '../../../../public/assets/studios';
 
 @Component({
   selector: 'app-discover',
   standalone: true,
-  imports: [RouterLink],
+  imports: [SliderComponent],
   templateUrl: './discover.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class DiscoverComponent { // Mock data signals
+export class DiscoverComponent { 
 
+
+  MOVIE_GENRES = MOVIE_GENRES;
+  SERIES_GENRES = SERIES_GENRES;
+  STUDIOS = STUDIOS;
+  
   tmdbService = inject(TmdbService);
+
   popularSeries: Signal<Series[]> = signal<Series[]>([])
+  trendingSeries: Signal<Series[]> = signal<Series[]>([])
+  
+  trendingMovies: Signal<Movie[]> = signal<Movie[]>([])
+  upcomingMovies: Signal<Movie[]> = signal<Movie[]>([])
+
 
   currentPopularSeriesPage = signal(1)
-
-
-
-  popularMovies = signal([
-    { name: 'Mufasa', poster_path: '/path/to/mufasa.jpg', release_date: '2024-12-01' },
-    { name: 'Superman', poster_path: '/path/to/superman.jpg', release_date: '2024-11-15' },
-    { name: 'Kraven', poster_path: '/path/to/kraven.jpg', release_date: '2024-10-10' },
-  ]);
-
-  trendingMovies = signal([
-    { name: 'Red One', poster_path: '/path/to/redone.jpg', release_date: '2024-09-01' },
-    { name: 'Carry-On', poster_path: '/path/to/carryon.jpg', release_date: '2024-08-20' },
-  ]);
-
-  movieGenres = signal(['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary']);
-
-  movieStudios = signal(['Walt Disney Pictures', '20th Century Studios', 'Sony Pictures', 'Warner Bros.', 'Universal', 'Paramount']);
 
   // Using existing service function for popular series
   ngOnInit() {
     this.popularSeries = this.tmdbService.popularSeries;
+    this.trendingSeries = this.tmdbService.trendingSeries;
+
+    this.trendingMovies = this.tmdbService.trendingMovies;
+    this.upcomingMovies = this.tmdbService.upcomingMovies;
     console.log(this.popularSeries())
   }
-  trendingSeries = signal([
-    { name: 'Queen', poster_path: '/path/to/queen.jpg', first_air_date: '2024-12-10' },
-    { name: 'Daredevil', poster_path: '/path/to/daredevil.jpg', first_air_date: '2024-11-01' },
-  ]);
 
-  seriesGenres = signal(['Action & Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama']);
-
-  seriesNetworks = signal(['Netflix', 'Disney+', 'Prime Video', 'Apple TV+', 'Hulu', 'HBO']);
 }
