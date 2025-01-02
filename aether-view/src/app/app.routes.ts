4db18155e6df99aa719c059e1dc9929adff5f738
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-03 00:57:25 +0100

    Refactor routing to use lazy loading for components and add filter routes for movies and series

--- aether-view/src/app/app.routes.ts
+++ aether-view/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
-import { DiscoverComponent } from './pages/discover/discover.component';
-import { TvDetailsComponent } from './pages/tv-details/tv-details.component';
 import { AppComponent } from './app.component';
-import { RecommendationComponent } from './pages/recommendation/recommendation.component';
-import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'recommendation', component: RecommendationComponent },
-  { path: 'discover', component: DiscoverComponent},
-  { path: 'tv/:id', component: TvDetailsComponent },
-  { path: 'movie/:id', component: MovieDetailsComponent }
+  { path: '', loadComponent: () => import('./pages/discover/discover.component').then(m => m.DiscoverComponent) },
+  { path: 'recommendation', loadComponent: () => import('./pages/recommendation/recommendation.component').then(m => m.RecommendationComponent) },
+  { path: 'discover/movies', loadComponent: () => import('./pages/filter-page/filter-page.component').then(m => m.FilterPageComponent), data: { type: 'movies' } }, // Filter movies
+  { path: 'discover/series', loadComponent: () => import('./pages/filter-page/filter-page.component').then(m => m.FilterPageComponent), data: { type: 'series' } }, // Filter series
+  { path: 'tv/:id', loadComponent: () => import('./pages/tv-details/tv-details.component').then(m => m.TvDetailsComponent) },
+  { path: 'movie/:id', loadComponent: () => import('./pages/movie-details/movie-details.component').then(m => m.MovieDetailsComponent) },
+
+
 ];
