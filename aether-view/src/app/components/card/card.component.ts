d4966c83bde364f0e75fecc62da17a7a42fec940
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-03 00:56:16 +0100

    Enhance card component to support item click navigation and route to movie or series details

--- aether-view/src/app/components/card/card.component.ts
+++ aether-view/src/app/components/card/card.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TmdbService } from '../../services/tmdb.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -15,10 +17,25 @@ export class CardComponent {
   @Input() type: 'movieSeries' | 'person' | 'genreStudio' | 'studio' = 'movieSeries'; // Card type
   @Input() overview?: string | null = null; // Overview/Summary
   @Input() maxOverviewLength?: number = 100;
+  @Input() item?: number | null = null; // ID of the item
+
+  tmdbService = inject(TmdbService);
+  router = inject(Router);
 
   getTruncatedText(text: string | null | undefined, maxLength: number): string {
     if (!text) return ''; // Handle null or undefined gracefully
     if (text.length <= maxLength) return text;
     return text.substring(0, maxLength) + '...';
+  }
+
+  onItemClicked(): void {
+    console.log('Item clicked:', this.item);
+    if (this.tmdbService.isSeries(this.item)) {
+      this.router.navigate(['/tv', this.item.id]);
+    } else if (this.tmdbService.isMovie(this.item)) {
+      this.router.navigate(['/movie', this.item.id]);
+    } else {
+      console.warn('Unknown item type:', this.item);
+    }
   }
 }
