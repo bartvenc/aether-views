05a39424c6a37a30f43cc05b111a44be5adc94b7
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-06 19:05:01 +0100

    Add person details component with profile and filmography display

---
+++ aether-view/src/app/pages/person-details/person-details.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, inject, Signal, signal, computed } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TmdbService } from '../../services/tmdb.service';
+import { Person } from '../../interfaces/common-interfaces';
+import { Movie } from '../../interfaces/movies';
+import { Series } from '../../interfaces/series';
+import { CardComponent } from '../../components/card/card.component';
+
+@Component({
+  selector: 'app-person-details',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './person-details.component.html',
+})
+export class PersonDetailsComponent implements OnInit {
+  route = inject(ActivatedRoute);
+  tmdbService = inject(TmdbService);
+
+  person = signal<Person | null>(null);
+  movies = signal<Movie[]>([]);
+  series = signal<Series[]>([]);
+
+  ngOnInit(): void {
+    const personId = this.route.snapshot.params['id'];
+
+    // Fetch person details
+    this.tmdbService.getPersonDetails(personId).subscribe(person => {
+      this.person.set(person);
+    });
+
+    // Fetch combined credits and split into movies and series
+    this.tmdbService.getPersonCombinedCredits(personId).subscribe(credits => {
+      const movies = credits.filter(credit => credit.media_type === 'movie') as Movie[];
+      const series = credits.filter(credit => credit.media_type === 'tv') as Series[];
+      this.movies.set(movies);
+      this.series.set(series);
+    });
+  }
+
+  get backdropImage(): string | null {
+    return this.person()?.profile_path ? this.tmdbService.getPosterUrl(this.person()?.profile_path) : null;
+  }
+
+  get hasMovies(): boolean {
+    return this.movies().length > 0;
+  }
+
+  get hasSeries(): boolean {
+    return this.series().length > 0;
+  }
+}
