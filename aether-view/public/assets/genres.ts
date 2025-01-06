a33b0b52f0a7cec298044775393b2c08156805c2
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-06 19:03:48 +0100

    Add genre type to movie and series genre definitions

--- aether-view/public/assets/genres.ts
+++ aether-view/public/assets/genres.ts
@@ -5,96 +5,115 @@ export const MOVIE_GENRES: Genre[] = [
     id: 28,
     name: 'Action',
     posterUrl: 'https://image.tmdb.org/t/p/w500/iheet74M0ktRbQiZY2abyciSBwv.jpg', // John Wick
+    type: 'movie',
   },
   {
     id: 12,
     name: 'Adventure',
     posterUrl: 'https://image.tmdb.org/t/p/w500/uLtVbjvS1O7gXL8lUOwsFOH4man.jpg', // Guardians of the Galaxy
+    type: 'movie',
   },
   {
     id: 16,
     name: 'Animation',
     posterUrl: 'https://image.tmdb.org/t/p/w500/4JaVWzbHr8l0phTzT7OyedsN0ia.jpg', // Minions
+    type: 'movie',
   },
   {
     id: 35,
     name: 'Comedy',
     posterUrl: 'https://image.tmdb.org/t/p/w500/sV49hOlUky6AhVYl4K0d3rergTA.jpg', // The Hangover
+    type: 'movie',
   },
   {
     id: 80,
     name: 'Crime',
     posterUrl: 'https://image.tmdb.org/t/p/w500/rSPw7tgCH9c6NqICZef4kZjFOQ5.jpg', // The Godfather
+    type: 'movie',
   },
   {
     id: 99,
     name: 'Documentary',
     posterUrl: 'https://image.tmdb.org/t/p/w500/wV0ZK0FkTMcf0rbxgCYMMeeGCd8.jpg', // Our Planet
+    type: 'movie',
   },
   {
     id: 18,
     name: 'Drama',
     posterUrl: 'https://image.tmdb.org/t/p/w500/eOZjrZWk8v1JizjbKycr4bXLh5b.jpg', // The Green Mile
+    type: 'movie',
   },
   {
     id: 10751,
     name: 'Family',
     posterUrl: 'https://image.tmdb.org/t/p/w500/hPzIyx60PhAHilGkuRr2zR5qlF2.jpg', // Bethoven
+    type: 'movie',
   },
   {
     id: 14,
     name: 'Fantasy',
     posterUrl: 'https://image.tmdb.org/t/p/w500/bVmSXNgH1gpHYTDyF9Q826YwJT5.jpg', // The Hobbit
+    type: 'movie',
   },
   {
     id: 36,
     name: 'History',
     posterUrl: 'https://image.tmdb.org/t/p/w500/2WgieNR1tGHlpJUsolbVzbUbE1O.jpg', // 1917
+    type: 'movie',
   },
   {
     id: 27,
     name: 'Horror',
     posterUrl: 'https://image.tmdb.org/t/p/w500/fWhAWcHdIKg08vjQsh9Nx3s8Hrm.jpg', // Halloween
+    type: 'movie',
   },
   {
     id: 10402,
     name: 'Music',
     posterUrl: 'https://image.tmdb.org/t/p/w500/5JKu78RZxxOHOJMn4bE6VdCb0hE.jpg', // Whiplash
+    type: 'movie',
   },
   {
     id: 9648,
     name: 'Mystery',
     posterUrl: 'https://image.tmdb.org/t/p/w500/4HWAQu28e2yaWrtupFPGFkdNU7V.jpg', // Knives Out
+    type: 'movie',
   },
   {
     id: 10749,
     name: 'Romance',
     posterUrl: 'https://image.tmdb.org/t/p/w500/pgEWI7yGYF0mM5Uf1YxSOHsBkJR.jpg', // Pride & Prejudice
+    type: 'movie',
   },
   {
     id: 878,
     name: 'Science Fiction',
     posterUrl: 'https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg', // Interstellar
+    type: 'movie',
   },
   {
     id: 10770,
     name: 'TV Movie',
     posterUrl: 'https://image.tmdb.org/t/p/w500/9uprGMyeVoeYX5rWurqzbOPXqtY.jpg', // The Day After Tomorrow
+    type: 'movie',
   },
   {
     id: 53,
     name: 'Thriller',
     posterUrl: 'https://image.tmdb.org/t/p/w500/fj0hwDJEOOHllRim2BMt5L7tbjf.jpg', // Leon
+    type: 'movie',
   },
   {
     id: 10752,
     name: 'War',
     posterUrl: 'https://image.tmdb.org/t/p/w500/pTwF9hLkqAtuOqXMdOyPwz4AgnI.jpg', // Dunkirk
+    type: 'movie',
   },
   {
     id: 37,
     name: 'Western',
     posterUrl: 'https://image.tmdb.org/t/p/w500/ltkWGokEIToUYyoUqc6PBI1kHPx.jpg', // Django Unchained
+    type: 'movie',
   },
 ];
 
@@ -103,80 +122,96 @@ export const SERIES_GENRES: Genre[] = [
     id: 10759,
     name: 'Action & Adventure',
     posterUrl: 'https://image.tmdb.org/t/p/w500/nGkSd7a05AbUjd8d48jiGyvyXl1.jpg', // The Witcher
+    type: 'series',
   },
   {
     id: 16,
     name: 'Animation',
     posterUrl: 'https://image.tmdb.org/t/p/w500/8aCek7W6BovH7M4enWjqrGptvQ8.jpg', // Rick and Morty
+    type: 'series',
   },
   {
     id: 35,
     name: 'Comedy',
     posterUrl: 'https://image.tmdb.org/t/p/w500/l0qVZIpXtIo7km9u5Yqh0nKPOr5.jpg', // Friends
+    type: 'series',
   },
   {
     id: 80,
     name: 'Crime',
     posterUrl: 'https://image.tmdb.org/t/p/w500/63FA8vwSZnXkGxedrDQwni4JuZN.jpg', // Breaking Bad
+    type: 'series',
   },
   {
     id: 99,
     name: 'Documentary',
     posterUrl: 'https://image.tmdb.org/t/p/w500/qmRUwosX4BsCj6QanpBYScLl3Sp.jpg', // Planet Earth
+    type: 'series',
   },
   {
     id: 18,
     name: 'Drama',
     posterUrl: 'https://image.tmdb.org/t/p/w500/aWnEtYJn31swY2pC8cOtje98bYc.jpg', // The Crown
+    type: 'series',
   },
   {
     id: 10751,
     name: 'Family',
     posterUrl: 'https://image.tmdb.org/t/p/w500/9bupbDyuQgJEAd9qGkXP54nIDtK.jpg', // Modern Family
+    type: 'series',
   },
   {
     id: 10762,
     name: 'Kids',
     posterUrl: 'https://image.tmdb.org/t/p/w500/ooIYvpusdiXzULeUlL1Ul7XljQK.jpg', // Peppa Pig
+    type: 'series',
   },
   {
     id: 9648,
     name: 'Mystery',
     posterUrl: 'https://image.tmdb.org/t/p/w500/dZklTql88IDOmkC3JAYQSTgyK6f.jpg', // Twin Peaks
+    type: 'series',
   },
   {
     id: 10763,
     name: 'News',
     posterUrl: 'https://image.tmdb.org/t/p/w500/w0GA9tmKfRgxXj2zPUp0qzDuYjC.jpg', // Last Week Tonight with John Oliver
+    type: 'series',
   },
   {
     id: 10764,
     name: 'Reality',
     posterUrl: 'https://image.tmdb.org/t/p/w500/sSmRyoMfgW498qHwYsQ9rhM6215.jpg', // Survivor
+    type: 'series',
   },
   {
     id: 10765,
     name: 'Sci-Fi & Fantasy',
     posterUrl: 'https://image.tmdb.org/t/p/w500/zZqpAXxVSBtxV9qPBcscfXBcL2w.jpg', // Game of Thrones
+    type: 'series',
   },
   {
     id: 10766,
     name: 'Soap',
     posterUrl: 'https://image.tmdb.org/t/p/w500/e7MSbwYRVqd4rZQQWcNuJvOSHLC.jpg', // Days of Our Lives
+    type: 'series',
   },
   {
     id: 10767,
     name: 'Talk',
     posterUrl: 'https://image.tmdb.org/t/p/w500/pVFWcrzPfoa6MxluwxZKrYsf9VU.jpg', // The Tonight Show Starring Jimmy Fallon
+    type: 'series',
   },
   {
     id: 10768,
     name: 'War & Politics',
     posterUrl: 'https://image.tmdb.org/t/p/w500/o6ixncGLOtRPz78vEIy9p1cOn9J.jpg', // The West Wing
+    type: 'series',
   },
   {
     id: 37,
     name: 'Western',
     posterUrl: 'https://image.tmdb.org/t/p/w500/2Erj4Oav9EHAtqLI354VM7ULDqu.jpg', // Yellowstone
+    type: 'series',
   },
 ];
