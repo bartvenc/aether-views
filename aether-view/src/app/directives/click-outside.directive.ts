3e377c41fc4113d0a525155ac31c2772b2cc6a4c
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-07 22:10:31 +0100

    Add ClickOutsideDirective to handle clicks outside of specified elements

---
+++ aether-view/src/app/directives/click-outside.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+
+@Directive({
+  selector: '[appClickOutside]'
+})
+export class ClickOutsideDirective {
+  @Output() appClickOutside: EventEmitter<{ context: string }> = new EventEmitter();
+  @Input() triggerElement!: HTMLElement;
+  @Input() context!: string; // Optional context to differentiate instances
+
+  private touchEventTriggered = false;
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('document:touchstart', ['$event.target'])
+  public onTouchStart(targetElement: HTMLElement): void {
+    this.touchEventTriggered = true;
+    this.handleOutsideClick(targetElement);
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  public onClick(targetElement: HTMLElement): void {
+    if (!this.touchEventTriggered) {
+      this.handleOutsideClick(targetElement);
+    }
+    this.touchEventTriggered = false;
+  }
+
+  private handleOutsideClick(targetElement: HTMLElement): void {
+    const clickedInside = this.elementRef.nativeElement.contains(targetElement);
+    const clickedTrigger = this.triggerElement?.contains(targetElement);
+    console.log('Clicked inside:', clickedInside, 'Clicked trigger:', clickedTrigger);
+    if (!clickedInside && !clickedTrigger) {
+      this.appClickOutside.emit({ context: this.context });
+    }
+  }
+}
