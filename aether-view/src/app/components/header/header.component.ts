07e6e51f9130b7a4d9b9874e4babdf02f4f99d42
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-07 19:08:09 +0100

    Refactor header component to enhance mobile navigation and add menu toggle functionality

--- aether-view/src/app/components/header/header.component.ts
+++ aether-view/src/app/components/header/header.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = false;
+}
