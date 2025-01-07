173a550375b763904c51c10d821ba9e957eacf2d
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-07 16:30:46 +0100

    Changed BACKEND_BASE_URL

---
+++ aether-view/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    BACKEND_BASE_URL: 'https://2326-85-165-128-86.ngrok-free.app', // Netlify backend
+  };
+  
--- aether-view/src/environments/environment.ts
+++ aether-view/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    BACKEND_BASE_URL: 'https://40ff-85-165-128-86.ngrok-free.app', // Local backend
+    BACKEND_BASE_URL: 'https://2326-85-165-128-86.ngrok-free.app', // Local backend
   };
   
