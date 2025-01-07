56ef4f0edbec262941db4a560b48fb3a6610a1d4
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-07 12:57:07 +0100

    Refactor backend service to use environment variable for base URL

--- aether-view/src/app/services/backend.service.ts
+++ aether-view/src/app/services/backend.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({ providedIn: 'root' })
 export class BackendService {
-  private baseUrl = 'http://localhost:5000'; // Adjust the URL if different
+  private backEndBaseUrl = environment.BACKEND_BASE_URL; // Adjust the URL if different
 
   constructor(private http: HttpClient) {}
 
   getRecommendations(query: string) {
-    return this.http.post<{ recommendations: string[] }>(`${this.baseUrl}/recommend`, { query });
+    return this.http.post<{ recommendations: string[] }>(`${this.backEndBaseUrl}/recommend`, { query });
   }
 
   search(query: string) {
-    return this.http.post<{ results: string[] }>(`${this.baseUrl}/search`, { query });
+    return this.http.post<{ results: string[] }>(`${this.backEndBaseUrl}/search`, { query });
   }
 }
