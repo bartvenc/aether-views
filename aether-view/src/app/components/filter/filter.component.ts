3dd1e70b0438c1af07bf13cdf11b916df8fdfe3e
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-09 21:02:37 +0100

    Enhance filter component with responsive design and Angular Material integration

--- aether-view/src/app/components/filter/filter.component.ts
+++ aether-view/src/app/components/filter/filter.component.ts
@@ -1,27 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, inject, Input, Output, signal, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, signal, OnInit, HostListener } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { Network } from '../../interfaces/series';
 import { Studio } from '../../../../public/assets/studios';
 import { Genre } from '../../interfaces/common-interfaces';
 import { TmdbService } from '../../services/tmdb.service';
-import { ClickOutsideDirective } from '../../directives/click-outside.directive';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, ClickOutsideDirective],
+  imports: [   CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule],
   templateUrl: './filter.component.html',
 })
 export class FilterComponent implements OnInit {
-  @Input() type: 'movies' | 'series' = 'movies'; // Default type
+  @Input() type: 'movies' | 'series' | null = null; // Default type
   @Input() genres: Genre[] = [];
   @Input() years: number[] = [];
   @Input() studiosOrNetworks: Studio[] | Network[] = [];
   @Input() filters: Record<string, any> = {};
   @Output() filterChange = new EventEmitter<any>();
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.checkViewport();
+  }
 
   tmdbService = inject(TmdbService);
+  router = inject(Router);
 
   selectedCategory = signal('Popular');
   selectedYear = signal(0);
@@ -30,15 +46,14 @@ export class FilterComponent implements OnInit {
   keywordInput = signal('');
   keywordResults = signal<{ id: number; name: string }[]>([]);
   selectedKeywords = signal<{ id: number; name: string }[]>([]);
-  genreDropdownOpen = signal<boolean>(false);
-  desktopGenreDropdownOpen = signal<boolean>(false);
-  mobileGenreDropdownOpen = signal<boolean>(false);
-  selectedType = signal('movies');
-  mobileFiltersOpen: boolean = false; 
+  selectedTypeSignal = signal<'movies' | 'series' | null>(this.type);
+  showFilters = false;
+  isMobile = false;
 
   ngOnInit() {
+    this.checkViewport();
     console.log(' 5 Filter component initialized with:', this.filters);
-    this.selectedType.set(this.type);
+    this.selectedTypeSignal.set(this.filters['type'] || this.type);
     this.selectedCategory.set(this.filters['category'] || 'Popular');
     this.selectedYear.set(this.filters['year'] || 0);
     this.selectedGenres.set(this.filters['genres'] || []);
@@ -46,6 +61,7 @@ export class FilterComponent implements OnInit {
     const keywordParam = this.filters['keyword'] ? [this.filters['keyword']] : [];
     this.selectedKeywords.set(keywordParam);
     console.log('  6 Filter component initialized after with:', {
+      type: this.selectedTypeSignal(),
       category: this.selectedCategory(),
       year: this.selectedYear(),
       genres: this.selectedGenres(),
@@ -60,63 +76,33 @@ export class FilterComponent implements OnInit {
     if (this.selectedCategory() === 'New' && this.selectedYear() === 0) {
       this.selectedYear.set(new Date().getFullYear());
     }
-
-    this.filterChange.emit({
-      category: this.selectedCategory(),
-      year: this.selectedYear(),
-      genres: this.selectedGenres(),
-      studioOrNetwork: this.selectedStudiosOrNetworks(),
-      keywords: this.selectedKeywords().map(kw => kw.id),
-      type: this.selectedType(),
-    });
-    console.log('Filter from filter component updated:', {
-      type: this.type,
-      category: this.selectedCategory(),
-      year: this.selectedYear(),
-      genres: this.selectedGenres(),
-      keyword: this.selectedKeywords(),
-      studioOrNetwork: this.selectedStudiosOrNetworks(),
-    });
-  }
-
-
-  toggleMobileFilters() {
-    this.mobileFiltersOpen = !this.mobileFiltersOpen;
-  }
-
-  toggleGenreDropdown(type: string) {
-    if (type === 'desktop') {
-      this.desktopGenreDropdownOpen.update(state => !state);
-    } else if (type === 'mobile') {
-      this.mobileGenreDropdownOpen.update(state => !state);
-    }
-  }
-
-  closeGenreDropdown(type: string) {
-    if(this.genreDropdownOpen() === true){
-      this.genreDropdownOpen.update(() => false);
-      console.log('Genre ', type, 'dropdown close:', this.genreDropdownOpen());
-    }
   
-  }
-
-  handleOutsideClick(event: { context: string }): void {
-    console.log('Outside click event:', event);
-    if (event.context === 'desktop') {
-      this.desktopGenreDropdownOpen.update(() => false);
-    } else if (event.context === 'mobile') {
-      this.mobileGenreDropdownOpen.update(() => false);
+    const newType = this.selectedTypeSignal();
+    if (newType !== this.type) {
+      this.router.navigate([`/discover/${newType}`], { queryParams: this.filters });
+    } else {
+      this.filterChange.emit({
+        type: this.selectedTypeSignal(),
+        category: this.selectedCategory(),
+        year: this.selectedYear(),
+        genres: this.selectedGenres(),
+        studioOrNetwork: this.selectedStudiosOrNetworks(),
+        keywords: this.selectedKeywords().map(kw => kw.id),
+      });
+      console.log('Filter from filter component updated:', {
+        type: this.selectedTypeSignal(),
+        category: this.selectedCategory(),
+        year: this.selectedYear(),
+        genres: this.selectedGenres(),
+        keyword: this.selectedKeywords(),
+        studioOrNetwork: this.selectedStudiosOrNetworks(),
+      });
     }
   }
 
-  updateSelectedGenres(genreId: number, isChecked: boolean) {
-    this.selectedGenres.update(currentGenres => {
-      if (isChecked) {
-        return [...currentGenres, genreId];
-      } else {
-        return currentGenres.filter(id => id !== genreId);
-      }
-    });
+  onGenreChange(event: any) {
+    // event.value is an array of selected IDs
+    this.selectedGenres.set(event.value);
     this.updateFilter();
   }
 
@@ -132,16 +118,22 @@ export class FilterComponent implements OnInit {
   }
 
   addKeyword(keyword: { id: number; name: string }) {
-    if (!this.selectedKeywords().find(kw => kw.id === keyword.id)) {
-      this.selectedKeywords.update(keywords => [...keywords, keyword]);
+    const exists = this.selectedKeywords().some(kw => kw.id === keyword.id);
+    if (!exists) {
+      this.selectedKeywords.update(current => [...current, keyword]);
+      this.keywordInput.set('');
+      this.keywordResults.set([]);
       this.updateFilter();
     }
-    this.keywordResults.set([]); // Clear results after selection
-    this.keywordInput.set(''); // Clear input
   }
 
   removeKeyword(keywordId: number) {
     this.selectedKeywords.update(keywords => keywords.filter(kw => kw.id !== keywordId));
     this.updateFilter();
   }
+
+  private checkViewport(): void {
+    this.isMobile = window.innerWidth < 768; // Tailwind's `md` breakpoint
+  }
+  
 }
