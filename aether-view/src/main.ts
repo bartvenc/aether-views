bd6d8060325fd02d1b40f3759202daa9c8fdb6bd
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-09 21:02:19 +0100

    Add async animations provider to bootstrap configuration

--- aether-view/src/main.ts
+++ aether-view/src/main.ts
@@ -6,8 +6,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import 'zone.js';
 import { register as registerSwiperElements } from 'swiper/element/bundle';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 registerSwiperElements();
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)],
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule), provideAnimationsAsync()],
 }).catch(err => console.error(err));
