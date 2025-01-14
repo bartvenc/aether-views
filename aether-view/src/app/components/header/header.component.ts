5049b0dd74f48741fbb3e5b0154ccc726a17a28c
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-14 23:39:32 +0100

    Refactor header component: update mobile menu structure, enhance accessibility, and improve imports for better functionality

--- aether-view/src/app/components/header/header.component.ts
+++ aether-view/src/app/components/header/header.component.ts
@@ -1,13 +1,25 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [
+    RouterLink,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+  ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
   isMenuOpen = false;
+  searchQuery = ''; // Placeholder for the search input value
 }
