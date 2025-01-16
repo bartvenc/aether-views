a521f9ea036b6fa1100da87384212c25b1406e62
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-16 11:28:41 +0100

    grok api

--- aether-view/src/environments/environment.prod.ts
+++ aether-view/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    BACKEND_BASE_URL: 'https://d110-85-165-128-86.ngrok-free.app', // Netlify backend
+    BACKEND_BASE_URL: 'https://3e97-85-165-128-86.ngrok-free.app',
   };
   
