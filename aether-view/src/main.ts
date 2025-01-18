fb1c0fbe69be8121ac0c7c89aeb7a040118ff06f
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-01-19 00:37:58 +0100

    Enhance application bootstrap: add app initializer to initialize TMDB service region

--- aether-view/src/main.ts
+++ aether-view/src/main.ts
@@ -1,14 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, inject, provideAppInitializer } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import 'zone.js';
 import { register as registerSwiperElements } from 'swiper/element/bundle';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TmdbService } from './app/services/tmdb.service';
 
 registerSwiperElements();
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes), 
+    importProvidersFrom(HttpClientModule), 
+    provideAnimationsAsync(),
+    provideAppInitializer(() => {
+      const tmdbService = inject(TmdbService);
+      return tmdbService.initializeRegion();
+    }),
+  ],
 }).catch(err => console.error(err));
